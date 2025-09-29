69136b17780810ad1b9bc07d22e900c3224d5717
آسال <telebotimport@gmail.com>
2025-09-29 14:10:00 +0300

    Uploaded Successfully

---
+++ FallenMusic/Modules/تطوير.py
@@ -0,0 +1,35 @@
+import asyncio
+import random
+import os
+import time
+import requests
+from random import  choice, randint
+from pyrogram import Client, filters
+from FallenMusic.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from FallenMusic import app
+
+
+@app.on_message(
+    command(["سورس","السورس","المطور"])
+    & filters.group
+)
+async def maker(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://graph.org/file/94fa4bb62424ea712eaa2.jpg",
+        caption=f"""-| مطور السورس \n-| قناة المطور""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "- مطور السورس .", url=f"https://t.me/rr8r9"),
+                ],
+                [
+                   InlineKeyboardButton(
+                        "- قناة المطور ", url=f"https://t.me/xl444"),
+                ],       
+            ]
+        ),
+    )
+
+
